eaa75f8b39e83d975fcbd5a98bff55cb701db2d5
michella galich <mgalich1@veleri.hr>
2026-05-14 17:17:31 +0200

    Dodana Django aplikacija i povezivanje s bazom

--- itpodrska/views.py
+++ itpodrska/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def pocetna(request):
+    return HttpResponse("""
+    <h1>Aplikacija za IT podršku</h1>
+    <p>Dobrodošli u sustav za evidenciju zahtjeva, incidenata, IT opreme, baze znanja i KPI izvještaja.</p>
+
+    <ul>
+        <li><a href="/admin/">Django admin</a></li>
+    </ul>
+    """)

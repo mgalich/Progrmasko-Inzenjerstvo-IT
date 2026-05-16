ee6cdcbf9c6d0d8eb57c8de17539b688acd4c0c8
michella galich <mgalich1@veleri.hr>
2026-05-16 19:03:35 +0200

    Dodani dashboard i navigacija aplikacije

--- itpodrska/urls.py
+++ itpodrska/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.pocetna, name='pocetna'),
+    path('zahtjevi/', views.zahtjevi, name='zahtjevi'),
+    path('incidenti/', views.incidenti, name='incidenti'),
+    path('oprema/', views.oprema, name='oprema'),
+    path('baza-znanja/', views.baza_znanja, name='baza_znanja'),
+    path('izvjestaji/', views.izvjestaji, name='izvjestaji'),
 ]
--- itpodrska/views.py
+++ itpodrska/views.py
@@ -1,11 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def pocetna(request):
-    return HttpResponse("""
-    <h1>Aplikacija za IT podršku</h1>
-    <p>Dobrodošli u sustav za evidenciju zahtjeva, incidenata, IT opreme, baze znanja i KPI izvještaja.</p>
+    return render(request, 'itpodrska/dashboard.html')
 
-    <ul>
-        <li><a href="/admin/">Django admin</a></li>
-    </ul>
-    """)
+from .models import Zahtjev, Incident, ItOprema, ClanakBazeZnanja, Izvjestaj
+
+def zahtjevi(request):
+    svi_zahtjevi = Zahtjev.objects.all()
+    return render(request, 'itpodrska/zahtjevi.html', {'zahtjevi': svi_zahtjevi})
+
+
+def incidenti(request):
+    svi_incidenti = Incident.objects.all()
+    return render(request, 'itpodrska/incidenti.html', {'incidenti': svi_incidenti})
+
+def oprema(request):
+    sva_oprema = ItOprema.objects.all()
+    return render(request, 'itpodrska/oprema.html', {'oprema': sva_oprema})
+
+def baza_znanja(request):
+    clanci = ClanakBazeZnanja.objects.all()
+    return render(request, 'itpodrska/baza_znanja.html', {'clanci': clanci})
+
+def izvjestaji(request):
+    svi_izvjestaji = Izvjestaj.objects.all()
+    return render(request, 'itpodrska/izvjestaji.html', {'izvjestaji': svi_izvjestaji})
+
